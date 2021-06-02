39b89a19dbbd1ceb562b1b366273683694bcefca
vesche <vesche@protonmail.com>
2021-06-02 14:37:01 -0500

    Update with game parsing logic

--- main.py
+++ main.py
@@ -8,12 +8,37 @@ from sanic import Sanic, response
 tag = 'mylobot-mark-i'
 app = Sanic(name=tag)
 
+class Game:
+    def __init__(self):
+        self.turn = int()
+        self.height = int()
+        self.width = int()
+        self.opponents = list()
+        self.food = list()
+        self.health = int()
+        self.body = list()
+        self.head = dict()
+        self.length = int()
+
+    def process_incoming(self, request):
+        self.turn = request.json['turn']
+        self.height = request.json['board']['height']
+        self.width = request.json['board']['width']
+        self.opponents = [s for s in request.json['board']['snakes'] if s['name'] != tag]
+        self.food = request.json['board']['food']
+        self.health = request.json['you']['health']
+        self.body = request.json['you']['body']
+        self.head = request.json['you']['head']
+        self.length = request.json['you']['length']
+
+game = Game()
+
 @app.route('/', methods=['GET',])
 async def index(request):
     return response.json({
         'apiversion': '1',
         'author': 'vesche',
-        'color': '#FAEBD7',
+        'color': '#FF69FF',
         'head': 'fang',
         'tail': 'curled',
         'version': tag,
@@ -21,20 +46,21 @@ async def index(request):
 
 @app.route('/start', methods=['POST',])
 async def start(request):
-    print(request.json)
+    process_incoming(request)
+    print('****** START', game.__dict__)
     return response.json({}, status=200)
 
 @app.route('/move', methods=['POST',])
 async def move(request):
-    print(request.json)
+    process_incoming(request)
+    print('****** MOVE', game.__dict__)
     direction = random.choice(['up', 'down', 'left', 'right'])
     shout = f'I am moving {direction}!'
     return response.json({'move': direction, 'shout': shout}, status=200)
 
 @app.route('/end', methods=['POST',])
 async def end(request):
-    print(request.json)
-    # tear down here?
+    print('****** END', request.json)
     return response.json({}, status=200)
 
 def run():
