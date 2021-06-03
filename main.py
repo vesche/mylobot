4217a8dba6bb0db23c32332e53141d5bd502ee8f
vesche <vesche@protonmail.com>
2021-06-03 10:47:55 -0500

    cleanup mark i

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ from sanic import Sanic, response
 
 tag = 'mylobot-mark-i'
 app = Sanic(name=tag)
+bot_meta = {
+    'apiversion': '1',
+    'author': 'vesche',
+    'color': '#FF69FF',
+    'head': 'fang',
+    'tail': 'curled',
+    'version': tag,
+}
 
 class Coord:
     def __init__(self, x, y):
@@ -20,21 +28,34 @@ class Coord:
 
 class Game:
     def __init__(self):
+        self.shout = str()
         self.stack = list()
 
     def process_incoming(self, request):
-        self.turn = request.json['turn']
+        self.turn   = request.json['turn']
         self.height = request.json['board']['height']
-        self.width = request.json['board']['width']
-        self.opponents = [s for s in request.json['board']['snakes'] if s['name'] != tag]
-        self.opponents_coords = [Coord(c['x'], c['y']) for c in sum([opponent['body'] for opponent in self.opponents], [])]
-        self.food = request.json['board']['food']
+        self.width  = request.json['board']['width']
+        self.food   = request.json['board']['food']
         self.health = request.json['you']['health']
-        self.body = request.json['you']['body']
-        self.body_coords = [Coord(c['x'], c['y']) for c in self.body]
         self.head_x = request.json['you']['head']['x']
         self.head_y = request.json['you']['head']['y']
         self.length = request.json['you']['length']
+
+        # my snake
+        self.body = request.json['you']['body']
+        self.body_coords = [Coord(c['x'], c['y']) for c in self.body]
+
+        # other snakes
+        self.opponents = [
+            i for i in request.json['board']['snakes'] if i['name'] != tag
+        ]
+        self.opponents_coords = [
+            Coord(c['x'], c['y']) for c in sum(
+                [opponent['body'] for opponent in self.opponents], list()
+            )
+        ]
+
+        # directional data
         self.coordinates = dict(
             up = Coord(self.head_x, self.head_y+1),
             down = Coord(self.head_x, self.head_y-1),
@@ -46,8 +67,10 @@ class Game:
         if self.stack:
             return self.stack.pop(0)
         ac = self.anti_collision()
-        # OR logic here...
-        return ac
+        # TODO: OR logic here...
+        direction = ac
+        self.shout = f'I am moving {direction}!'
+        return direction
 
     def anti_collision(self):
         # evaluate collisions
@@ -62,7 +85,7 @@ class Game:
             )):
                 coord.collision = True
 
-        print(self.coordinates)
+        print('*** COORDS ***', self.coordinates)
 
         # random (based on board size)
         if random.choice(range(9)) == 3:
@@ -77,32 +100,23 @@ game = Game()
 
 @app.route('/', methods=['GET',])
 async def index(request):
-    return response.json({
-        'apiversion': '1',
-        'author': 'vesche',
-        'color': '#FF69FF',
-        'head': 'fang',
-        'tail': 'curled',
-        'version': tag,
-    })
+    return response.json(bot_meta)
 
 @app.route('/start', methods=['POST',])
 async def start(request):
     game.process_incoming(request)
-    # print('****** START', game.__dict__)
+    print('*** START ***', game.__dict__)
     return response.json({}, status=200)
 
 @app.route('/move', methods=['POST',])
 async def move(request):
     game.process_incoming(request)
-    # print('****** MOVE', game.__dict__)
-    direction = game.runner()
-    shout = f'I am moving {direction}!'
-    return response.json({'move': direction, 'shout': shout}, status=200)
+    print('*** MOVE ***', game.__dict__)
+    return response.json({'move': game.runner(), 'shout': game.shout}, status=200)
 
 @app.route('/end', methods=['POST',])
 async def end(request):
-    # print('****** END', request.json)
+    print('*** END ***', request.json)
     return response.json({}, status=200)
 
 def run():
